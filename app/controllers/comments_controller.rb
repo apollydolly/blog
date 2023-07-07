a4460253480a1a32cbd709a4c5b3172bd1375823
apollydolly <polina0106028049@gmail.com>
2023-07-07 21:17:58 +0300

    first commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,15 @@ class CommentsController < ApplicationController
   #http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
   before_action :authenticate_user!, except: [:destroy]
   def create
-    @article = Article.find(params[:article_id])
+    # @article = Article.find(params[:article_id])
+    @article = Article.find_by(title: params[:article_title].gsub('-', ' '))
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
+    # @article = Article.find(params[:article_id])
+    @article = Article.find_by(title: params[:article_title].gsub('-', ' '))
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article), status: :see_other
